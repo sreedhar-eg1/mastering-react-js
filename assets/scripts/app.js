bf5549a6fec02ea26ffab3c60f81c90006dc7316
sreedhar eg <sreedhareg1997@gmail.com>
2025-02-14 16:23:25 +0530

    javascript import export

---
+++ assets/scripts/app.js
@@ -0,0 +1,9 @@
+// Default import
+// import apiKey from './util.js'
+// console.log(apiKey);
+
+// import {apiKey1, apiKey2} from './util.js'
+// console.log(apiKey1, apiKey2);
+
+import * as apiKey from './util.js'
+console.log(apiKey.apiKey1, apiKey.apiKey2);
---
+++ assets/scripts/util.js
@@ -0,0 +1,5 @@
+// Default export - one default export per file
+// export default 'dhasfhsgfgshf'
+
+export let apiKey1 = 'sdjhjfjhsfj'
+export const apiKey2 = 'fsdgdsghh'
