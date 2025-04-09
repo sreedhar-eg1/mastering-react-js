1b113199579737ae970a125e22b4f58238e75421
sreedhar eg <sreedhareg1997@gmail.com>
2025-04-09 11:49:58 +0530

    completed authentication

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,3 +1,4 @@
+import { redirect } from 'react-router-dom';
 import AuthForm from '../components/AuthForm';
 
 function AuthenticationPage() {
@@ -5,3 +6,47 @@ function AuthenticationPage() {
 }
 
 export default AuthenticationPage;
+
+export async function action({request, params}) {
+  const searchParams = new URL(request.url).searchParams
+  const mode = searchParams.get('mode') || 'login'
+
+  const data = await request.formData()
+
+  if (mode !== 'login' && mode !== 'signup') {
+    throw new Response(JSON.stringify({message: 'Invalid mode'}), {status: 422})
+  }
+
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password')
+  }
+
+  const response = await fetch(`http://localhost:8080/${mode}`, {
+    method: request.method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(authData)
+  })
+
+  if (response.status === 422 || response.status === 401) {
+    return response
+  }
+
+  if (!response.ok) {
+    throw new Response(JSON.stringify({message: 'Could not authenticate user.'}), {status: 500})
+  }
+
+  // manage token
+  const resData = await response.json()
+  const token = resData.token
+
+  localStorage.setItem('token', token)
+  const expiration = new Date()
+  expiration.setHours(expiration.getHours() + 1)
+  localStorage.setItem('expiration', expiration.toISOString())
+
+  // redirect
+  return redirect('/')
+}
--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,38 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import {
+  Outlet,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
+} from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../util/Auth";
 
 function RootLayout() {
   // const navigation = useNavigation();
+  // with the help of useLoaderData hook, we can get the data which was returned from the loader function
+  const token = useLoaderData();
+  // with the help of usesubmit hook, we can programatically submit form
+  const submit = useSubmit();
+
+  // this is not a ideal solution for automatic logout
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration()
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
 
   return (
     <>
